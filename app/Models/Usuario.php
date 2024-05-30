63e071cd838b5c9d389d73d150be87196aa6c22c
José Carlos Ramírez Rodríguez <carlosuthti@gmail.com>
2024-05-30 11:00:42 -0700

    Creacion de CRUD de usuarios

---
+++ app/Models/Usuario.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Usuario
+ *
+ * @property $id
+ * @property $nombre
+ * @property $apellido
+ * @property $correo
+ * @property $created_at
+ * @property $updated_at
+ *
+ * @package App
+ * @mixin \Illuminate\Database\Eloquent\Builder
+ */
+class Usuario extends Model
+{
+    
+    protected $perPage = 20;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = ['nombre', 'apellido', 'correo'];
+
+
+}
--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\UsuarioController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    print "Hola Soy Jesus Alfredo Figueroa Silvas,  Este Framework de Laravel 11 ya esta funcionando";
-    return view('welcome');
-});
+
+
+Route::resource('usuarios', UsuarioController::class);
+Route::get('/register', 'Auth\RegisterController@showRegistrationForm');
+
+
