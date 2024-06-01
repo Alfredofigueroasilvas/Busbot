f3af729e638972abc17d6902d0fb6096ea054df3
Carlos Cubillas <150886077+carcub@users.noreply.github.com>
2024-06-01 02:56:49 -0700

    intalacion y configuracion
    intalacion y configuracion de laravel breeze para login, registro y envio de e mail de verificacion

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    print "Hola Soy Jesus Alfredo Figueroa Silvas,  Este Framework de Laravel 11 ya esta funcionando";
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
