18e6c4c30beda73451cf909bd867fe325559f282
Carlos Cubillas <150886077+carcub@users.noreply.github.com>
2024-07-26 18:41:18 -0700

    Configuración de Inicio de sesión issue #4 (#23)
    * configuración de breeze
    
    * establecer rutas
    
    * reparación de archivo index.php para sun funcionamiento correcto de la aplicación
    
    * inicio de sesión, registrar, recuperar contraseña, ruta protegida y envío de correos
    
    * refactorización de breeze

--- routes/web.php
+++ routes/web.php
@@ -1,20 +1,30 @@
 <?php
 
-use App\Http\Controllers\UsuarioController;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\MapController;
+use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\UsuarioController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
 
-Route::get('/map', [MapController::class, 'showMap']);
+
+Route::get('map', MapController::class);
 Route::resource('usuarios', UsuarioController::class);
-Route::get('/register', 'Auth\RegisterController@showRegistrationForm');
-
-
-
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
 
+Route::get('dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
 
-Route::get('/enviar-notificacion', function () {
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('enviar-notificacion', function() {
     return view('emails.notificar');
 });
+
+require __DIR__.'/auth.php';
